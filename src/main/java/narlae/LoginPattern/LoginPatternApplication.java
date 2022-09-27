7b40703cc641a2da1e86144e17b8e2575c7eca7e
narlae <kyjttaa12@gmail.com>
2022-09-27 16:09:26 +0900

    Initial commit

---
+++ src/main/java/narlae/LoginPattern/LoginPatternApplication.java
@@ -0,0 +1,13 @@
+package narlae.LoginPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/narlae/LoginPattern/LoginPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package narlae.LoginPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
